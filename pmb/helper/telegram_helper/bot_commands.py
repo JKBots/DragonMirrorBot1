497726d28c67eca37f810cab392bfe11eccf45a0
Ishan-EXE <84384195+Ishan-EXE@users.noreply.github.com>
2021-06-07 15:24:21 +0530

    Initial commit

---
+++ pmb/helper/telegram_helper/bot_commands.py
@@ -0,0 +1,28 @@
+class _BotCommands:
+    def __init__(self):
+        self.StartCommand = 'start'
+        self.MirrorCommand = 'mirror6'
+        self.UnzipMirrorCommand = 'extract6'
+        self.TarMirrorCommand = 'tar6'
+        self.CancelMirror = 'cancel6'
+        self.CancelAllCommand = 'cancelall6'
+        self.ListCommand = 'list'
+        self.StatusCommand = 'status'
+        self.AuthorizeCommand = 'auth6'
+        self.UnAuthorizeCommand = 'unauth6'
+        self.AuthListCommand = 'authlist6'
+        self.PingCommand = 'ping'
+        self.RestartCommand = 'restart6'
+        self.StatsCommand = 'stats'
+        self.HelpCommand = 'help'
+        self.LogCommand = 'log'
+        self.SpeedCommand = 'speedtest'
+        self.CloneCommand = 'clone'
+        self.WatchCommand = 'ytdl'
+        self.TarWatchCommand = 'playlist'
+        self.DeleteCommand = 'del'
+        self.UsageCommand = 'usage'
+        self.RepoCommand = 'repo6'
+        self.ConfigCommand = 'config'
+
+BotCommands = _BotCommands()
